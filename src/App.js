73599c25ca1d2951cbcec002377f26442f4eb184
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-06-01 19:53:34 -0400

    Setting up React, added Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Contact from "./components/Contact";
+import Aboutme from "./components/Aboutme";
+import Portfolio from "./components/Portfolio";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Navbar />
+        <Contact />
+        <Portfolio />
+        <Aboutme />
+      </div>
     </div>
   );
 }
---
+++ src/components/Aboutme.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Aboutme() {
+  return (
+    <div>
+      <p>About Me</p>
+    </div>
+  );
+}
+
+export default Aboutme;
