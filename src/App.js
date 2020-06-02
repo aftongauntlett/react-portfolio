7cb820dd6cca0526f3219d00bfab30907b071c93
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-06-01 20:43:28 -0400

    adding router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Home from "./components/views/Home";
 import Navbar from "./components/shared/Navbar";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/views/Home";
+import Aboutme from "./components/views/Aboutme";
+import Portfolio from "./components/views/Portfolio";
+import Contact from "./components/views/Contact";
 
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
+    <Router>
+      <Navbar />
       <div>
-        <Navbar />
-        <Home />
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <Aboutme />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
-
-export default App;
--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -10,29 +10,15 @@ import {
 
 function myNavbar() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="/">Home</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="/about">About Me</Nav.Link>
+          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
       </Navbar.Collapse>
     </Navbar>
   );
